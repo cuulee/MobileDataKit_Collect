3e2b3495643e44f7ee4a17dded40122fcf9bfa50
Ayubu Masasi <ayubumasasi@gmail.com>
2018-05-18 14:41:19 +0300

    Added FormElement class

---
+++ MobileDataKit_Collect.Common/FormElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileDataKit_Collect.Common
+{
+   public class FormElement :  Realms.RealmObject
+    {
+
+        
+        public string Type { get; set; }
+
+        public  string Name { get; set; }
+
+
+        public string Label { get; set; }
+
+        public string Relevant { get; set; }
+
+
+        public IList<FormElement> Fields { get; }
+    }
+}
